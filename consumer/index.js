51d87a5376b39bb05073724b548e05d612c1c93f
Brian Blocker <blocker.brian@gmail.com>
2015-08-14 00:10:02 -0500

    Consumer Logging + Tests
    * Added setOptions method (no tests yet)
    * Added logResults method + tests
    * Added writeLogsToFile method + tests

--- consumer/index.js
+++ consumer/index.js
@@ -1,6 +1,14 @@
-function Consumer () {
+var fs = require('fs');
 
+function Consumer (options) {
+  this.setOptions(options);
 }
+
+Consumer.prototype.setOptions = function setOptions (options) {
+  options = options || Object.create(null);
+
+  this.options = options;
+};
 
 Consumer.prototype.insertIntoCache = function insertIntoCache (expression, result) {
   Consumer.cache[expression] = result;
@@ -14,6 +22,36 @@ Consumer.prototype.validateExpression = function validateExpression (expression)
   return /^\d+((\+|\-|\*)\d+)+=$/.test(String(expression));
 };
 
+Consumer.prototype.logResults = function logResults (message) {
+  var timestamp = (new Date()).toISOString();
+
+  Consumer.logs.push([timestamp, message].join(': '));
+};
+
+Consumer.prototype.writeLogsToFile = function writeLogsToFile () {
+  var new_message = '';
+  var tmp;
+
+  while (tmp = Consumer.logs.shift()) {
+    new_message += tmp + '\n';
+  }
+
+  if (! this.options.writeLogs) {
+    console.log(new_message);
+
+    return;
+  }
+
+  fs.appendFile(__dirname + '/log.txt', new_message, function (err) {
+    if (err) {
+      console.log(err);
+    }
+
+    setTimeout(this.writeLogsToFile, 10000);
+  }.bind(this));
+};
+
 Consumer.cache = {};
+Consumer.logs  = [];
 
 module.exports = Consumer;
